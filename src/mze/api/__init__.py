959a810f9ef84c9bf0f26ba42c1818313818cf01
Maksym Medvied <maksym.medvied@gmail.com>
2021-09-02 23:31:26 +0300

    mze.api: export more from .storage

--- src/mze/api/__init__.py
+++ src/mze/api/__init__.py
@@ -1,7 +1,16 @@
+from .storage import BlobId
+from .storage import BlobInfo
+from .storage import BlobData
+from .storage import Storage
 from .storage import StorageServer
 from .storage import StorageClient
 
 __all__ = [
+    'BlobId',
+    'BlobInfo',
+    'BlobData',
+
+    'Storage',
     'StorageServer',
     'StorageClient',
 ]
