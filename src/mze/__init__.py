db764a31a18d5d988219a4f003950dddbc429c06
Maksym Medvied <maksym.medvied@gmail.com>
2021-09-02 23:31:04 +0300

    mze: don't reexport mze.api.*

--- src/mze/__init__.py
+++ src/mze/__init__.py
@@ -1,11 +1,5 @@
-from mze.api import StorageServer
-from mze.api import StorageClient
-
 from mze.storage_server.dir import StorageServerDir
 
 __all__ = [
-    'StorageServer',
-    'StorageClient',
-
     'StorageServerDir',
 ]
