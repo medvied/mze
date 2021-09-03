9110b6b48558e5aba2efb95e24a524adaa474bb3
Maksym Medvied <maksym.medvied@gmail.com>
2021-09-03 22:06:17 +0300

    mze/api/__init__: add extra empty line after copyright header

--- src/mze/api/__init__.py
+++ src/mze/api/__init__.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from .storage import BlobId
 from .storage import BlobInfo
 from .storage import BlobData
