639f5632bd3da36ae57c668854750575421cab9c
Maksym Medvied <maksym.medvied@gmail.com>
2024-12-29 14:22:17 -0500

    src/container: add new() to create dyn Container by name

--- src/container.rs
+++ src/container.rs
@@ -1,1 +1,15 @@
+use std::error;
+
+use crate::Container;
+
 pub mod sqlite;
+
+pub fn new(
+    name: &str,
+    uri: &str,
+) -> Result<Box<dyn Container>, Box<dyn error::Error>> {
+    Ok(Box::new(match name {
+        "sqlite" => sqlite::ContainerSqlite::new(uri)?,
+        _ => panic!("container::new(): unknown name={name}"),
+    }))
+}
