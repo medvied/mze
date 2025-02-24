178b912a3a07d0c1503e2d8a0d84965337da3980
Maksym Medvied <maksym.medvied@gmail.com>
2025-02-23 22:13:10 -0500

    src/container/ram: add no-op implementation

--- src/container.rs
+++ src/container.rs
@@ -2,14 +2,16 @@ use std::error;
 
 use crate::Container;
 
+pub mod ram;
 pub mod sqlite;
 
 pub fn new(
     name: &str,
     uri: &str,
 ) -> Result<Box<dyn Container + Send>, Box<dyn error::Error>> {
-    Ok(Box::new(match name {
-        "sqlite" => sqlite::ContainerSqlite::new(uri)?,
+    Ok(match name {
+        "sqlite" => Box::new(sqlite::ContainerSqlite::new(uri)?),
+        "ram" => Box::new(ram::ContainerRam::new(uri)?),
         _ => panic!("container::new(): unknown name={name}"),
-    }))
+    })
 }
---
+++ src/container/ram.rs
@@ -0,0 +1,158 @@
+use std::{
+    collections::{HashMap, HashSet},
+    error, iter,
+};
+
+use log::{debug, error};
+
+use crate::{
+    Container, ContainerTransaction, EntityId, Link, Record, SearchQuery,
+    SearchResult, ENTITY_ID_START,
+};
+
+pub struct ContainerRam {}
+pub struct ContainerRamTransaction {}
+
+impl Container for ContainerRam {
+    fn new(uri: &str) -> Result<Self, Box<dyn error::Error>>
+    where
+        Self: Sized,
+    {
+        Ok(Self {})
+    }
+
+    fn create(&self) -> Result<(), Box<dyn error::Error>> {
+        Ok(())
+    }
+
+    fn destroy(&self) -> Result<(), Box<dyn error::Error>> {
+        Ok(())
+    }
+
+    fn load(&self, uri: String) {}
+
+    fn save(&self, uri: String) {}
+
+    fn begin_transaction(
+        &mut self,
+    ) -> Result<Box<dyn ContainerTransaction + '_>, Box<dyn error::Error>>
+    {
+        Ok(Box::new(ContainerRamTransaction {}))
+    }
+}
+
+impl ContainerTransaction for ContainerRamTransaction {
+    fn commit(self: Box<Self>) -> Result<(), Box<dyn error::Error>> {
+        Ok(())
+    }
+
+    fn rollback(self: Box<Self>) -> Result<(), Box<dyn error::Error>> {
+        Ok(())
+    }
+
+    fn search(
+        &self,
+        search_query: &SearchQuery,
+    ) -> Result<Vec<SearchResult>, Box<dyn error::Error>> {
+        Ok(Vec::new())
+    }
+
+    fn tags_get(
+        &self,
+        eid: &EntityId,
+    ) -> Result<Vec<String>, Box<dyn error::Error>> {
+        Ok(Vec::new())
+    }
+
+    fn tags_put(
+        &mut self,
+        eid: &EntityId,
+        tags: &[String],
+    ) -> Result<(), Box<dyn error::Error>> {
+        Ok(())
+    }
+
+    fn tags_del(
+        &mut self,
+        eid: &EntityId,
+    ) -> Result<(), Box<dyn error::Error>> {
+        Ok(())
+    }
+
+    fn attributes_get(
+        &self,
+        eid: &EntityId,
+    ) -> Result<Vec<(String, String)>, Box<dyn error::Error>> {
+        Ok(Vec::new())
+    }
+
+    fn attributes_put(
+        &mut self,
+        eid: &EntityId,
+        attributes: &[(String, String)],
+    ) -> Result<(), Box<dyn error::Error>> {
+        Ok(())
+    }
+
+    fn attributes_del(
+        &mut self,
+        eid: &EntityId,
+    ) -> Result<(), Box<dyn error::Error>> {
+        Ok(())
+    }
+
+    fn record_get(
+        &self,
+        eid: &EntityId,
+    ) -> Result<Option<Record>, Box<dyn error::Error>> {
+        Ok(None)
+    }
+
+    fn record_put(
+        &mut self,
+        eid: &Option<EntityId>,
+        record: &Record,
+    ) -> Result<EntityId, Box<dyn error::Error>> {
+        Ok(EntityId::new(0))
+    }
+
+    fn record_del(
+        &mut self,
+        eid: &EntityId,
+    ) -> Result<bool, Box<dyn error::Error>> {
+        Ok(false)
+    }
+
+    fn record_get_all_ids(
+        &self,
+    ) -> Result<Vec<EntityId>, Box<dyn error::Error>> {
+        Ok(Vec::new())
+    }
+
+    fn link_get(
+        &self,
+        eid: &EntityId,
+    ) -> Result<Option<Link>, Box<dyn error::Error>> {
+        Ok(None)
+    }
+    fn link_put(
+        &mut self,
+        eid: &Option<EntityId>,
+        link: &Link,
+    ) -> Result<EntityId, Box<dyn error::Error>> {
+        Ok(EntityId::new(0))
+    }
+
+    fn link_del(
+        &mut self,
+        eid: &EntityId,
+    ) -> Result<bool, Box<dyn error::Error>> {
+        Ok(false)
+    }
+
+    fn link_get_all_ids(
+        &self,
+    ) -> Result<Vec<EntityId>, Box<dyn error::Error>> {
+        Ok(Vec::new())
+    }
+}
